7e9a24b26dd18d7d924c4fab0a144faff89eea8d
Amandine4731 <5618@holbertonstudents.com>
2024-04-19 02:20:56 -0700

    Basic setup task 1 work locally

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+import _ from 'lodash';
+
+
+$(document).ready(function (){
+    $("<p>Holberton Dashboard</p>").appendTo(document.body);
+    $("<p>Dashboard data for the students</p>").appendTo(document.body);
+    $("<p>Copyright - Holberton School</p>").appendTo(document.body);
+})
