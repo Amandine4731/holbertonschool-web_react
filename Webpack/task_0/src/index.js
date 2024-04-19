821d5d217df1da9e99c7f6252c864d017d36b07e
Amandine4731 <5618@holbertonstudents.com>
2024-04-19 02:54:43 -0700

    format document

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,7 +1,7 @@
 import $ from "jquery";
 
 
-$(document).ready(function (){
+$(document).ready(function () {
     $("<p>Holberton Dashboard</p>").appendTo(document.body);
     $("<p>Dashboard data for the students</p>").appendTo(document.body);
     $("<p>Copyright - Holberton School</p>").appendTo(document.body);
